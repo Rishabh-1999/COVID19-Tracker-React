6934c74884f968b23d5789cccb596b7b760baa7d
Arihant Jain <jainanilkumar739@gmail.com>
2020-03-29 18:40:48 +0530

    High and low cases part completed...

--- src/Components/FavStates/FavStates.tsx
+++ src/Components/FavStates/FavStates.tsx
@@ -1,70 +1,144 @@
 import React from "react";
 
 function FavListItem(props :any) {
-  const ddata =
-    props.states.length > 0
-      ? props.states.map((state :any) => {
-          return (
-            <div
-              className="card m-2"
-              key={state.loc}
-              style={{ width: "300px" }}
-            >
-              <h5 className="card-header btn bg-primary">
-                <i className="fa fa-star"/>
-                {state.loc}
-              </h5>
-              <div className="card-body">
-                <ul className="list-group">
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                    Total
-                    <span className="badge badge-primary badge-pill">
-                      {state.total}
-                    </span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                    Confirmed Cases Indian
-                    <span className="badge badge-primary badge-pill">
-                      {state.confirmedCasesIndian}
-                    </span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                    Confirmed Cases Foreign
-                    <span className="badge badge-primary badge-pill">
-                      {state.confirmedCasesForeign}
-                    </span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                    Discharged
-                    <span className="badge badge-primary badge-pill">
-                      {state.discharged}
-                    </span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                    Deaths
-                    <span className="badge badge-primary badge-pill">
-                      {state.deaths}
-                    </span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          );
-        })
-      : "";
+    const ddata =
+        props.states.length > 0
+            ? props.states.map((state :any) => {
+                return (
+                    <div
+                        className="card m-2"
+                        key={state.loc}
+                        style={{ width: "300px" }}
+                    >
+                        {
+                            state.high === true ?
+                                <h5 className="card-header btn bg-danger text-white">
+                                    <i className="fa fa-star"/>
+                                    {state.loc}
+                                </h5>
+                                :
+                                state.low === true ?
+                                    <h5 className="card-header btn bg-warning">
+                                        <i className="fa fa-star"/>
+                                        {state.loc}
+                                    </h5>
+                                    :
+                                    <h5 className="card-header btn bg-primary">
+                                        <i className="fa fa-star"/>
+                                        {state.loc}
+                                    </h5>
+                        }
+                        <div className="card-body">
+                            <ul className="list-group">
+                                <li className="list-group-item d-flex justify-content-between align-items-center">
+                                    Total
+                                    {
+                                        state.high === true ?
+                                            <span className="badge badge-danger badge-pill">
+                                                {state.total}
+                                            </span>
+                                            :
+                                            state.low === true ?
+                                                <span className="badge badge-warning badge-pill">
+                                                    {state.total}
+                                                </span>
+                                                :
+                                                <span className="badge badge-primary badge-pill">
+                                                  {state.total}
+                                                </span>
+                                    }
+                                </li>
+                                <li className="list-group-item d-flex justify-content-between align-items-center">
+                                    Confirmed Cases Indian
+                                    {
+                                        state.high === true ?
+                                            <span className="badge badge-danger badge-pill">
+                                                {state.confirmedCasesIndian}
+                                            </span>
+                                            :
+                                            state.low === true ?
+                                                <span className="badge badge-warning badge-pill">
+                                                    {state.confirmedCasesIndian}
+                                                </span>
+                                                :
+                                                <span className="badge badge-primary badge-pill">
+                                                    {state.confirmedCasesIndian}
+                                                </span>
+                                    }
+                                </li>
+                                <li className="list-group-item d-flex justify-content-between align-items-center">
+                                    Confirmed Cases Foreign
+                                    {
+                                        state.high === true ?
+                                            <span className="badge badge-danger badge-pill">
+                                                {state.confirmedCasesForeign}
+                                            </span>
+                                            :
+                                            state.low === true ?
+                                                <span className="badge badge-warning badge-pill">
+                                                    {state.confirmedCasesForeign}
+                                                </span>
+                                                :
+                                                <span className="badge badge-primary badge-pill">
+                                                    {state.confirmedCasesForeign}
+                                                </span>
+                                    }
+                                </li>
+                                <li className="list-group-item d-flex justify-content-between align-items-center">
+                                    Discharged
+                                    {
+                                        state.high === true ?
+                                            <span className="badge badge-danger badge-pill">
+                                                {state.discharged}
+                                            </span>
+                                            :
+                                            state.low === true ?
+                                                <span className="badge badge-warning badge-pill">
+                                                    {state.discharged}
+                                                </span>
+                                                :
+                                                <span className="badge badge-primary badge-pill">
+                                                    {state.discharged}
+                                                </span>
+                                    }
+                                </li>
+                                <li className="list-group-item d-flex justify-content-between align-items-center">
+                                    Deaths
+                                    {
+                                        state.high === true ?
+                                            <span className="badge badge-danger badge-pill">
+                                                {state.deaths}
+                                            </span>
+                                            :
+                                            state.low === true ?
+                                                <span className="badge badge-warning badge-pill">
+                                                    {state.deaths}
+                                                </span>
+                                                :
+                                                <span className="badge badge-primary badge-pill">
+                                                    {state.deaths}
+                                                </span>
+                                    }
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                );
+            })
+            : "";
 
-  return (
-    props.states.length !== 0 && (
-      <div className="container mb-lg-5">
-        <div>
-          <h2>
-            <u>Favourite State</u>
-          </h2>
-        </div>
-        <div className="row w-100 mt-2">{ddata}</div>
-      </div>
-    )
-  );
+    return (
+        props.states.length !== 0 && (
+            <div className="container mb-lg-5">
+                <div>
+                    <h2>
+                        <u>Favourite State</u>
+                    </h2>
+                </div>
+                <div className="row w-100 mt-2">{ddata}</div>
+            </div>
+        )
+    );
 }
 
 export default FavListItem;
--- src/Components/MainPart/MainPart.jsx
+++ src/Components/MainPart/MainPart.jsx
@@ -3,87 +3,112 @@ import { Container } from "react-bootstrap";
 import axios from "axios";
 import DataHeader from "../DataHeader/DataHeader";
 import List from "../List/List";
-import FavList from "../FavStates/FavStates";
+import FavListItem from "../FavStates/FavStates";
 
 class Mainpart extends Component {
-  state = {
-    summary: Object,
-    states: [],
-    favstates: []
-  };
+    state = {
+        summary: Object,
+        states: [],
+        favstates: []
+    };
 
-  componentDidMount() {
-    this.addToFav.bind(this);
-    this.getData();
-  }
-
-  async addTotalAndFav(d) {
-    d.map( (s) => {
-      s.fav = this.state.fav.find(loc => loc === s.loc) !== undefined;
-      s.total = s.confirmedCasesIndian + s.confirmedCasesForeign;
-      return s;
-    });
-    return d;
-  }
-
-  async manipulateData(res) {
-    res = await this.addTotalAndFav(res);
-    console.log(this.state);
-    let temp_states = this.state.states;
-    let temp_favstates = this.state.favstates;
-
-    for (let i = 0; i < res.length; i++) {
-      if (res[i].fav) {
-        temp_favstates.push(res[i]);
-      } else {
-        temp_states.push(res[i]);
-      }
+    componentDidMount() {
+        this.addToFav.bind(this);
+        this.getData();
     }
-    this.setState({ states: temp_states, favstates: temp_favstates });
-  }
 
-  async getData() {
-    let fav_temp = JSON.parse(
-        await window.localStorage.getItem("covidstatesfav")
-    );
-    if (fav_temp === undefined || fav_temp === null)
-      this.setState({ fav: [] });
-    else
-      this.setState({ fav: fav_temp });
-    await axios
-      .get("https://api.rootnet.in/covid19-in/stats/latest")
-      .then(res => {
-        return res.data.data;
-      })
-      .then(res => {
-        this.setState({ summary: res.summary });
-        this.manipulateData(res.regional);
-        //this.setState({ data: res.regional });
-      });
-  }
+    async addTotalAndFav(d) {
+        d.map(s => {
+            s.fav = this.state.fav.find(loc => loc === s.loc) !== undefined;
+            s.total = s.confirmedCasesIndian + s.confirmedCasesForeign;
+            return s;
+        });
+        return d;
+    }
 
-  async addToFav(event) {
-    this.state.fav.push(event);
-    window.localStorage.setItem(
-      "covidstatesfav",
-      JSON.stringify(this.state.fav)
-    );
-    window.location.reload();
-  }
+    async checkHighLow(res) {
+        let high = 0, highState, lowState, low = Number.MAX_SAFE_INTEGER;
+        res.map(state => {
+            if(high < state.total)
+            {
+                highState = state.loc;
+                high = state.total;
+            }
+            if(low > state.total)
+            {
+                low = state.total;
+                lowState = state.loc;
+            }
+            return state;
+        });
+        res.map(state => {
+            if(state.loc === highState)
+                state.high = true;
+            else if(state.loc === lowState)
+                state.low = true;
+            return state;
+        });
+        return res;
+    }
 
-  render() {
-    return (
-      <Container>
-        <DataHeader summary={this.state.summary} />
-        <FavList states={this.state.favstates} getData={this.getData} />
-        <List
-          states={this.state.states}
-          getData={this.getData}
-          addToFav={this.addToFav.bind(this)}
-        />
-      </Container>
-    );
-  }
+    async manipulateData(res) {
+        res = await this.checkHighLow(await this.addTotalAndFav(res));
+        // console.log(res);
+        let temp_states = this.state.states;
+        let temp_favstates = this.state.favstates;
+
+        for (let i = 0; i < res.length; i++) {
+            if (res[i].fav) {
+                temp_favstates.push(res[i]);
+            } else {
+                temp_states.push(res[i]);
+            }
+        }
+        this.setState({ states: temp_states, favstates: temp_favstates });
+    }
+
+    async getData() {
+        let fav_temp = JSON.parse(
+            await window.localStorage.getItem("covidstatesfav")
+        );
+        if (fav_temp === undefined || fav_temp === null)
+            this.setState({ fav: [] });
+        else
+            this.setState({ fav: fav_temp });
+        await axios
+            .get("https://api.rootnet.in/covid19-in/stats/latest")
+            .then(res => {
+                return res.data.data;
+            })
+            .then(res => {
+                this.setState({ summary: res.summary });
+                this.manipulateData(res.regional);
+                //this.setState({ data: res.regional });
+            });
+    }
+
+    async addToFav(event) {
+        this.state.fav.push(event);
+        window.localStorage.setItem(
+            "covidstatesfav",
+            JSON.stringify(this.state.fav)
+        );
+        window.location.reload();
+    }
+
+    render() {
+        return (
+            <Container>
+                <DataHeader summary={this.state.summary} />
+                <FavListItem states={this.state.favstates} getData={this.getData} />
+                <List
+                    states={this.state.states}
+                    getData={this.getData}
+                    addToFav={this.addToFav.bind(this)}
+                />
+            </Container>
+        );
+    }
 }
 
 export default Mainpart;
